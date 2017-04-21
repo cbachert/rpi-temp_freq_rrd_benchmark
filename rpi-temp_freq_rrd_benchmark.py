dffcc60f398cf5ffa8c4434b1d0ada555360fa5c
cbachert <cbachert@users.noreply.github.com>
2017-04-21 23:39:37 +0000

    Add graphing functionality

--- rpi-temp_freq_rrd_benchmark.py
+++ rpi-temp_freq_rrd_benchmark.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import rrdtool, tempfile
 import time
 import subprocess
 import re
 
 epoch_time = int(time.time())
-file_rrd = 'rpi-temp_freq-rrd.rrd'
+path_rrd = 'rpi-temp_freq.rrd'
+path_rrd_export = 'rpi-temp_freq.png'
+
 rrd_length = 30
+rrd_res = 1
+rrd_datapoints = rrd_length / rrd_res
 
 path_cpu_temp = '/sys/class/thermal/thermal_zone0/temp'
 path_cpu_freq = '/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq'
@@ -19,13 +25,13 @@ data_sources=[ 'DS:gpu_temp:GAUGE:2:0:128',
                'DS:cpu_temp:GAUGE:2:0:128',
                'DS:cpu_freq:GAUGE:2:0:1200000000' ]
 
-rrdtool.create( file_rrd,
+rrdtool.create( path_rrd,
                  '--start', str(epoch_time),
-                 '--step', '1',
+                 '--step', str(rrd_res),
                  data_sources,
-                 'RRA:AVERAGE:0.5:1:%s' %(rrd_length))
+                 'RRA:AVERAGE:0.5:%s:%s' %(rrd_res, rrd_datapoints))
 
-for i in range(1, rrd_length+1):
+for i in range(1, rrd_datapoints+1):
   # get CPU temperature
   file_cpu_temp = open(path_cpu_temp, 'r')
   cpu_temp = float(file_cpu_temp.readline().rstrip())/1000
@@ -34,6 +40,7 @@ for i in range(1, rrd_length+1):
   # get GPU temperature
   proc_get_gpu_temp = subprocess.Popen([path_gpu_temp, path_gpu_temp_arg], stdout=subprocess.PIPE)
   proc_get_gpu_temp_result = proc_get_gpu_temp.stdout.read()
+  print proc_get_gpu_temp_result
   gpu_temp = regex_gpu_temp.search(proc_get_gpu_temp_result).group(1)
 
   # get CPU frequency
@@ -43,8 +50,27 @@ for i in range(1, rrd_length+1):
 
   print 'gpu_temp: %s\ncpu_temp: %s\ncpu_freq: %s' %(gpu_temp, cpu_temp, cpu_freq)
   
-  rrdtool.update(file_rrd, 'N:%s:%s:%s' %(gpu_temp, cpu_temp, cpu_freq))
+  rrdtool.update(path_rrd, 'N:%s:%s:%s' %(gpu_temp, cpu_temp, cpu_freq))
 
-  time.sleep(1) 
+  time.sleep(rrd_res) 
 
+print path_rrd
 
+rrdtool.graph(path_rrd_export,
+              '--imgformat', 'PNG',
+              '--width', '960',
+              '--height', '540',
+              '--start', str(epoch_time),
+              '--end', "-1",
+              '--vertical-label', 'Temperature in C',
+              '--title', 'RPI Temperature Benchmark',
+              '--lower-limit', '0',
+              '--right-axis', '10000000:0',
+              '--right-axis-label', 'Frequency in Hz',
+              'DEF:rpi-gpu_temp=rpi-temp_freq.rrd:gpu_temp:AVERAGE',
+              'DEF:rpi-cpu_temp=rpi-temp_freq.rrd:cpu_temp:AVERAGE',
+              'DEF:rpi-cpu_freq=rpi-temp_freq.rrd:cpu_freq:AVERAGE',
+              'CDEF:scaled_rpi-cpu_freq=rpi-cpu_freq,10000,/',
+              'LINE1:rpi-gpu_temp#0000FF:"gpu_temp"',
+              'LINE1:rpi-cpu_temp#00FF00:"cpu_temp"',
+              'LINE1:scaled_rpi-cpu_freq#FF0000:"cpu_freq"')
